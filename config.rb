07b890a430428d2626978cb8641bca115a3081f8
Michel Kraemer <michel@undercouch.de>
2012-11-30 15:39:21 +0100

    Initial commit

---
+++ config.rb
@@ -0,0 +1,12 @@
+require 'zurb-foundation'
+
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "scripts"
+
+output_style = :compressed
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
